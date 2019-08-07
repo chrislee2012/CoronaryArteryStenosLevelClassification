c2f21aef3c0662abd6df7577afc90f5c3b08d5ed
Lewbolew <petryshak@ucu.edu.ua>
2019-08-07 14:53:25 +0200

    removed redundant comments from dataloader

--- dataloaders/binary_data_loader.py
+++ dataloaders/binary_data_loader.py
@@ -9,43 +9,6 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import cv2
 
-# class LAD_MPR_Loader(Dataset):
-
-#     def __init__(self, path_to_csv, root_dir, dataset_partition, augment=None):
-#         self.labels = pd.read_csv(path_to_csv)
-#         self.dataset_partition_name = dataset_partition # train test val
-#         self.root_dir = root_dir
-#         self.augment = augment
-#         self.data_transformations = transforms.Compose([
-#                 transforms.ToTensor(),
-#                 # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-#             ])
-#     def __len__(self):
-#         return len(self.labels)
-
-#     def __getitem__(self, idx):
-#         # print(self.labels.iloc[idx]['PATIENT_NAME'])
-#         artery_section_name = self._artery_secton_name_strip(self.labels.iloc[idx]['IMG_NAME'].split('_')[0])
-#         patient_folder_name = self.labels.iloc[idx]['PATIENT_NAME']
-#         image_name = self.labels.iloc[idx]['IMG_NAME']
-#         y = torch.tensor(self.labels.iloc[idx]['LABEL'], dtype=torch.long)
-#         stenosis_score = self.labels.iloc[idx]['STENOSIS_SCORE']
-#         img_path = os.path.join(self.root_dir, self.dataset_partition_name, patient_folder_name, artery_section_name, image_name)
-#         X =cv2.imread(img_path)# cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
-#         if self.augment:
-#             X = self.augment(image=X)['image']
-#         X = self.data_transformations(X)
-#         return X, y, stenosis_score, patient_folder_name, self.labels.iloc[idx]['IMG_NAME']
-
-#     def _artery_secton_name_strip(self, section_name):
-#         if 'D-2' in section_name:
-#             section_name = 'D-2'
-#         elif 'D-1' in section_name:
-#             section_name = 'D-1'
-#         elif 'LAD' in section_name:
-#             section_name = 'LAD'
-#         return section_name
-
 class Binary_MPR_Loader(Dataset):
 
     def __init__(self, root_dir, csv_name, dataset_partition, sample_each_nth_mpr_image=3, augment=None, transformations=None):
