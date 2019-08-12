e4712ddfddcff193999f8b57e00c3094ae7a5d5c
Lewbolew <bohdanpv@gmail.com>
2019-08-13 00:53:33 +0300

    merged sampler

--- datasets/sampler.py
+++ datasets/sampler.py
@@ -45,11 +45,7 @@ class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
         elif dataset_type is torchvision.datasets.ImageFolder:
             return dataset.imgs[idx][1]
         else:
-<<<<<<< HEAD:dataloaders/sampler.py
-            return dataset.labels.iloc[idx,6]
-=======
             return dataset.labels[idx]
->>>>>>> 496715200783f33699b46993821d0e0450acdda7:datasets/sampler.py
                 
     def __iter__(self):
         return (self.indices[i] for i in torch.multinomial(
