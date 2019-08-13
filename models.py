db6fe5934e63568721ccb8279a2f2c3c6b2842f9
Lewbolew <petryshak@ucu.edu.ua>
2019-08-13 13:20:04 +0200

    added SqueezeNet model

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from torch import nn
 from torchvision import models
-from efficientnet_pytorch import EfficientNet
 
 
 
@@ -24,14 +23,13 @@ class ResNet50(nn.Module):
     def forward(self, x):
         return self.model(x)
 
-class EfficientNetB0(nn.Module):
+class ShuffleNetv2(nn.Module):
 
-    def __init__(self, n_classes=2, pretrained=True)
-        super(EfficientNetB0, self).__init__()
-        if pretrained:
-            self.model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=n_classes)
-        else:
-            self.model = EfficientNet.from_name('efficientnet-b0')
+    def __init__(self, n_classes=2, pretrained=True):
+        super(ShuffleNetv2, self).__init__()
+        self.model =  models.shufflenet_v2_x2_0(pretrained=pretrained)
+        features_num = self.model.fc.in_features
+        self.model.fc = nn.Linear(features_num, n_classes)
 
     def forward(self, x):
         return self.model(x)
